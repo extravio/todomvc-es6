f21e5b7354852f1674a373f98c2ac068741f0755
Mikael <mikaelmr@gmail.com>
2019-09-30 22:35:07 +1300

    Rewrite app.js using the Elm architecture

--- js/app.js
+++ js/app.js
@@ -1,39 +1,33 @@
 import model from './model.js';
 
-class App {
+const update = () => {};
 
-	constructor(elementId) {
-		this.element = document.getElementById(elementId);
-	}
+const view = (signal, model, root) => {
+	let html = '<ul class="todo-list">';
+	html = model.reduce((acc, cur) => {
+		return acc + `<li class="${cur.completed ? 'completed' : ''}">
+						<div class="view">
+							<input class="toggle" type="checkbox" ${cur.completed ? 'checked' : ''}>
+							<label>${cur.task}</label>
+							<button class="destroy"></button>
+						</div>
+						<input class="edit" value="${cur.task}">
+					</li>`
+	}, html);
+	html += '</ul>';
 
-	setModel(model) {
-		this.model = model;
-	}
+	root.innerHTML = html;
+  }
 
-	render() {
-		//<!-- These are here just to show the structure of the list items -->
-		//<!-- List items should get the class 'editing' when editing and 'completed' when marked as completed -->
+const mount = (model, update, view, elementId) => {
+	const root = document.getElementById(elementId); // root DOM element
+	const signal = (action) => { 
+		() => {
+			model = update(model, action);
+			view(signal, model, root);
+		};
+	};
+	view(signal, model, root);         // render initial model (once)
+};
 
-		let html = '<ul class="todo-list">';
-		html = this.model.reduce((acc, cur) => {
-			return acc + `<li class="${cur.completed ? 'completed' : ''}">
-							<div class="view">
-								<input class="toggle" type="checkbox" ${cur.completed ? 'checked' : ''}>
-								<label>${cur.task}</label>
-								<button class="destroy"></button>
-							</div>
-							<input class="edit" value="${cur.task}">
-						</li>`
-		}, html);
-		html += '</ul>';
-
-		this.element.innerHTML = html;
-	}
-}
-
-export default App;
-
-
-let app = new App('app');
-app.setModel(model);
-app.render();
+mount(model, update, view, 'app');
