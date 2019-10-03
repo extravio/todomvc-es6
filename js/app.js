c0b3568f9a59b59b39ca5754adc153b9ac4f7c2c
mikael <mikael.mrozik@acc.co.nz>
2019-10-03 15:51:27 +1300

    Add clear-completed action

--- js/app.js
+++ js/app.js
@@ -17,6 +17,9 @@ const tmp_event = (signal, model, root) => {
 			}
 		})
 	});
+	root.querySelectorAll('button.clear-completed').forEach((element, index) => {
+		element.addEventListener('click', signal('clear-completed', null))
+	});
 }
 
 const update = (model, action, payload) => {
@@ -29,6 +32,7 @@ const update = (model, action, payload) => {
 		});
 		case 'destroy': return model.filter((item, index) => index!==payload);
 		case 'new-todo': return [{ task: payload, completed: false }, ...model];
+		case 'clear-completed': return model.filter((item) => !item.completed);
 		default: return model;           // if no action, return curent state.
 	  }
 };
@@ -36,28 +40,49 @@ const update = (model, action, payload) => {
 const view = (signal, model, root) => {
 	let html;
 	html = `
-		<header class="header">
-			<h1>todos</h1>
-			<input class="new-todo" placeholder="What needs to be done?" autofocus>
-		</header>
-		<!-- This section should be hidden by default and shown when there are todos -->
-		<section class="main">
-			<input id="toggle-all" class="toggle-all" type="checkbox" ${model.reduce((acc, cur) => acc && cur.completed, true) ? 'checked' : ''}>
-			${ model.length ? '<label for="toggle-all">Mark all as complete</label>' : '' }
-			<ul class="todo-list">
-				<!-- These are here just to show the structure of the list items -->
-				<!-- List items should get the class 'editing' when editing and 'completed' when marked as completed -->
-				${ model.map((item) => {
-					return `<li class="${item.completed ? 'completed' : ''}">
-										<div class="view">
-											<input class="toggle" type="checkbox" ${item.completed ? 'checked' : ''}>
-											<label>${item.task}</label>
-											<button class="destroy"></button>
-										</div>
-										<input class="edit" value="${item.task}">
-									</li>`
-					}).join('') }
-			</ul>
+		<section class="todoapp">
+			<header class="header">
+				<h1>todos</h1>
+				<input class="new-todo" placeholder="What needs to be done?" autofocus>
+			</header>
+			<!-- This section should be hidden by default and shown when there are todos -->
+			<section class="main">
+				<input id="toggle-all" class="toggle-all" type="checkbox" ${model.reduce((acc, cur) => acc && cur.completed, true) ? 'checked' : ''}>
+				${ model.length ? '<label for="toggle-all">Mark all as complete</label>' : '' }
+				<ul class="todo-list">
+					<!-- These are here just to show the structure of the list items -->
+					<!-- List items should get the class 'editing' when editing and 'completed' when marked as completed -->
+					${ model.map((item) => {
+						return `<li class="${item.completed ? 'completed' : ''}">
+											<div class="view">
+												<input class="toggle" type="checkbox" ${item.completed ? 'checked' : ''}>
+												<label>${item.task}</label>
+												<button class="destroy"></button>
+											</div>
+											<input class="edit" value="${item.task}">
+										</li>`
+						}).join('') }
+				</ul>
+			</section>
+			<!-- This footer should hidden by default and shown when there are todos -->
+			<footer class="footer">
+				<!-- This should be '0 items left' by default -->
+				<span class="todo-count"><strong>${ model.reduce((acc, cur) => acc + ((!cur.completed) ? 1 : 0), 0) }</strong> item left</span>
+				<!-- Remove this if you don't implement routing -->
+				<ul class="filters">
+					<li>
+						<a class="selected" href="#/">All</a>
+					</li>
+					<li>
+						<a href="#/active">Active</a>
+					</li>
+					<li>
+						<a href="#/completed">Completed</a>
+					</li>
+				</ul>
+				<!-- Hidden if no completed items are left ↓ -->
+				<button class="clear-completed">Clear completed</button>
+			</footer>
 		</section>`;
 	root.innerHTML = html;
 	tmp_event(signal, model, root);
